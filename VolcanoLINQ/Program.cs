72052bb9d3317df784c731a03aa2f88ec15d3be2
Chuck <chuck-bates@pluralsight.com>
2020-01-08 17:47:44 -0700

    Quantification examples

--- VolcanoLINQ/Program.cs
+++ VolcanoLINQ/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using VolcanoLINQ.Base;
 using VolcanoLINQ.Filter;
 using VolcanoLINQ.Order;
+using VolcanoLINQ.Quantification;
 using VolcanoLINQ.Syntax;
 
 namespace VolcanoLINQ
@@ -16,8 +17,14 @@ namespace VolcanoLINQ
             var volcanoes = Load();
 //            FilterExamples.WhereExample(volcanoes);
 //            FilterExamples.FirstOrDefaultExample(volcanoes);
+
 //            SyntaxExamples.CompareSyntax(volcanoes);
-            OrderExamples.OrderByDescendingExample(volcanoes);
+
+//            OrderExamples.OrderByDescendingExample(volcanoes);
+
+//            QuantificationExamples.AnyExample(volcanoes);
+//            QuantificationExamples.AllExample(volcanoes);
+            QuantificationExamples.CountExample(volcanoes);
             Console.ReadKey();
         }
 
---
+++ VolcanoLINQ/Quantification/QuantificationExamples.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using VolcanoLINQ.Base;
+
+namespace VolcanoLINQ.Quantification
+{
+    public static class QuantificationExamples
+    {
+        public static void AnyExample(IEnumerable<Volcano> volcanoes)
+        {
+            var result = volcanoes.Any(v => v.Type == "Stratovolcano");
+            Console.WriteLine($"Are there any of type Stratovolcano: {result}");
+
+            result = volcanoes.Any(v => v.Type == "Supervolcano");
+            Console.WriteLine($"Are there any of type Supervolcano: {result}");
+        }
+
+        public static void AllExample(IEnumerable<Volcano> volcanoes)
+        {
+            var result = volcanoes.All(v => v.PopulationClose > 0);
+            Console.WriteLine($"Are all volcanoes close to populations: {result}");
+
+            result = volcanoes.All(v => v.Type.Length > 0);
+            Console.WriteLine($"Are all volcanoes typed: {result}");
+        }
+
+        public static void CountExample(IEnumerable<Volcano> volcanoes)
+        {
+            var result = volcanoes.Count(v => v.Type == "Stratovolcano");
+            Console.WriteLine($"There are {result} known stratovolcanoes");
+
+            result = volcanoes.Count(v => v.Name == "Chuck");
+            Console.WriteLine($"There are {result} volcanoes with the name \"Chuck\"");
+        }
+    }
+}
