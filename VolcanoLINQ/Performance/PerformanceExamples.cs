feca8ac3dd9b07ede9c12d161e2fe9e4c055395c
Chuck <chuck-bates@pluralsight.com>
2020-01-09 13:29:57 -0700

    Update CompareToLoopsExample to not use any LINQ in loops

--- VolcanoLINQ/Performance/PerformanceExamples.cs
+++ VolcanoLINQ/Performance/PerformanceExamples.cs
@@ -15,15 +15,27 @@ namespace VolcanoLINQ.Performance
                 .OrderByDescending(v => v.PopulationClose)
                 .ThenByDescending(v => v.LastEruptionYear)
                 .Select(v => (v.Name, v.PopulationClose, v.Country, v.LastEruptionYear)).ToList();
-            var linqTime = stopwatch.ElapsedTicks;
+            var linqTime = stopwatch.ElapsedMilliseconds;
             stopwatch.Restart();
 
             var loopResults = new List<(string Name, int PopulationClose, string Country, int LastEruptionYear)>();
-            foreach (var v1 in volcanoes.Where(v => v.PopulationClose > 1000 && v.LastEruptionYear > DateTime.UtcNow.Year - 5)
-                .OrderByDescending(v => v.PopulationClose)
-                .ThenByDescending(v => v.LastEruptionYear))
-                loopResults.Add((v1.Name, v1.PopulationClose, v1.Country, v1.LastEruptionYear));
-            var loopTime = stopwatch.ElapsedTicks;
+            foreach (var volcano in volcanoes)
+            {
+                if (volcano.PopulationClose > 1000 && volcano.LastEruptionYear > DateTime.UtcNow.Year - 5)
+                {
+                    loopResults.Add((volcano.Name, volcano.PopulationClose, volcano.Country, volcano.LastEruptionYear));
+                }
+            }
+
+            loopResults.Sort(delegate((string Name, int PopulationClose, string Country, int LastEruptionYear) x, (string Name, int PopulationClose, string Country, int LastEruptionYear) y)
+            {
+                if (x.PopulationClose > y.PopulationClose)
+                {
+                    return -1;
+                }
+                return 1;
+            });
+            var loopTime = stopwatch.ElapsedMilliseconds;
 
             Console.WriteLine($"Results of linq and loop syntax are equal: {linqResults.SequenceEqual(loopResults)}");
             Console.WriteLine($"LINQ execution time: {linqTime}");
